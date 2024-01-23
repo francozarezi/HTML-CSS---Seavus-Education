de5141625aeb07bc2142356255b9c7117ff8545a
Franco Zarezi <franco_zarezi@yahoo.com>
2024-01-23 02:55:35 +0100

    JS

---
+++ JS - 01/script.js
@@ -0,0 +1,16 @@
+alert ("Cena telefona sa taksom od 5%")
+alert (typeof "Cena telefona sa taksom od 5%")
+
+var pricePerPhone = 119.95;
+var totalCost = pricePerPhone * 30;
+var totalTax = totalCost * 0.05
+var totalCostWithTax = totalTax + totalCost
+
+console.log(totalCostWithTax);
+
+var cenaJednogTelefona = 119.95
+var taksaZaJedanTelefon = cenaJednogTelefona * 0.05
+var cenaTelefonaSaTakson = cenaJednogTelefona + taksaZaJedanTelefon
+var ukupnaCenaTelefona = cenaTelefonaSaTakson * 30
+
+console.log(ukupnaCenaTelefona);
